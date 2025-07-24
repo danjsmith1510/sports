390b6b0b583adceefd31c05135727dea055e8cb0
Dan Smith <danieljsmith1510@gmail.com>
2025-07-24 17:13:54 +1000

    try this

--- flows/rotowire_flow.py
+++ flows/rotowire_flow.py
@@ -8,8 +8,10 @@ from tasks.rotowire import get_projected_minutes
 load_dotenv(verbose=True, override=True)
 league_active_nba = os.environ.get("league_active_nba")
 league_active_wnba = os.environ.get("league_active_wnba")
-nba_teams = os.environ.get("teams_nba")
-wnba_teams = os.environ.get("teams_wnba")
+teams_nba = os.environ.get("teams_nba")
+teams_wnba = os.environ.get("teams_wnba")
+nba_teams = [team.strip() for team in teams_nba.split(",")]
+wnba_teams = [team.strip() for team in teams_wnba.split(",")]
 
 @flow(log_prints=True)
 def rotowire_flow() -> str:
@@ -17,7 +19,6 @@ def rotowire_flow() -> str:
     if (league_active_nba == "False"):
         print("NBA league is not active, skipping projected minutes extraction")
     else:
-        nba_teams = [team.strip() for team in nba_teams.split(",")]
         projected_minutes_list_nba = get_projected_minutes(nba_teams)
         print(f"Got projected minutes for {len(wnba_teams)} nba teams")  
         insert_bronze_extracts("projected-mins-nba", json.dumps(projected_minutes_list_nba))
@@ -25,7 +26,6 @@ def rotowire_flow() -> str:
     if (league_active_wnba == "False"):
         print("WNBA league is not active, skipping projected minutes extraction")
     else:
-        wnba_teams = [team.strip() for team in wnba_teams.split(",")]
         projected_minutes_list_wnba = get_projected_minutes(wnba_teams)
         print(f"Got projected minutes for {len(wnba_teams)} wnba teams")  
         insert_bronze_extracts("projected-mins-wnba", projected_minutes_list_wnba)
